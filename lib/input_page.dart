0ff0ae013f79da50a20fec1b35431bf96d11fbad
Varshith Raj Basa <varshithraj.basa@gmail.com>
2020-08-14 14:14:31 +0530

    Added Varibles for more optimised code

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -17,20 +21,26 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: Row(
               children: <Widget>[
-                Expanded(child: ReusableCard(color1: Color(0xFF1D1E33))),
-                Expanded(child: ReusableCard(color1: Color(0xFF1D1E33))),
+                Expanded(child: ReusableCard(color1: activeCardColor)),
+                Expanded(child: ReusableCard(color1: activeCardColor)),
               ],
             ),
           ),
-          Expanded(child: ReusableCard(color1: Color(0xFF1D1E33))),
+          Expanded(child: ReusableCard(color1: activeCardColor)),
           Expanded(
             child: Row(
               children: [
-                Expanded(child: ReusableCard(color1: Color(0xFF1D1E33))),
-                Expanded(child: ReusableCard(color1: Color(0xFF1D1E33))),
+                Expanded(child: ReusableCard(color1: activeCardColor)),
+                Expanded(child: ReusableCard(color1: activeCardColor)),
               ],
             ),
           ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          )
         ],
       ),
     );
