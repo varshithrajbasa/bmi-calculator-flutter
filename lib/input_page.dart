b5cbcafa0fa8f069835007fd3dec9b3e1b5bd5d1
Varshith Raj Basa <varshithraj.basa@gmail.com>
2020-08-20 17:40:36 +0530

    Added Slider

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusable_card.dart';
 import 'icon_content.dart';
+import 'constants.dart';
 
-const bottomContainerHeight = 70.0;
-const activeCardColor = Color(0xFF1D1E33);
-const bottomContainerColor = Color(0xFFEB1555);
-const inactiveColor = Color(0xFF111328);
 enum Gender { male, female }
 
 class InputPage extends StatefulWidget {
@@ -16,6 +13,7 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender selectedGender;
+  int height = 180;
 
   @override
   Widget build(BuildContext context) {
@@ -24,69 +22,94 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI CALCULATOR'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: GestureDetector(
-                    onTap: () {
+                  child: ReusableCard(
+                    onPressed: () {
                       setState(() {
                         selectedGender = Gender.male;
                       });
                     },
-                    child: ReusableCard(
-                      color1: selectedGender == Gender.male
-                          ? activeCardColor
-                          : inactiveColor,
-                      customWidget: GestureDetector(
-                        child: IconContent(
-                            icon: FontAwesomeIcons.mars, label: 'MALE'),
-                      ),
-                    ),
+                    color1: selectedGender == Gender.male
+                        ? kActiveCardColor
+                        : kInactiveColor,
+                    customWidget:
+                        IconContent(icon: FontAwesomeIcons.mars, label: 'MALE'),
                   ),
                 ),
                 Expanded(
-                  child: GestureDetector(
-                    onTap: () {
+                  child: ReusableCard(
+                    onPressed: () {
                       setState(() {
                         selectedGender = Gender.female;
                       });
                     },
-                    child: ReusableCard(
-                      color1: selectedGender == Gender.female
-                          ? activeCardColor
-                          : inactiveColor,
-                      customWidget: GestureDetector(
-                        child: IconContent(
-                            icon: FontAwesomeIcons.venus, label: 'FEMALE'),
-                      ),
-                    ),
+                    color1: selectedGender == Gender.female
+                        ? kActiveCardColor
+                        : kInactiveColor,
+                    customWidget: IconContent(
+                        icon: FontAwesomeIcons.venus, label: 'FEMALE'),
                   ),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: ReusableCard(color1: activeCardColor),
+            child: ReusableCard(
+              color1: kActiveCardColor,
+              customWidget: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('HEIGHT', style: kLabelTextStyle),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    children: [
+                      Text(
+                        height.toString(),
+                        style: kNumberTextStyle,
+                      ),
+                      Text(
+                        'cm',
+                        style: kLabelTextStyle,
+                      )
+                    ],
+                  ),
+                  Slider(
+                    value: height.toDouble(),
+                    min: 120.0,
+                    max: 220.0,
+                    activeColor: Color(0xFFEB1555),
+                    inactiveColor: Color(0xFF8D8E98),
+                    onChanged: (double newValue) {
+                      print(newValue);
+                    },
+                  )
+                ],
+              ),
+            ),
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCard(color1: activeCardColor),
+                  child: ReusableCard(color1: kActiveCardColor),
                 ),
                 Expanded(
-                  child: ReusableCard(color1: activeCardColor),
+                  child: ReusableCard(color1: kActiveCardColor),
                 ),
               ],
             ),
           ),
           Container(
-            color: bottomContainerColor,
+            color: kBottomContainerColor,
             margin: EdgeInsets.only(top: 10.0),
             width: double.infinity,
-            height: bottomContainerHeight,
+            height: kBottomContainerHeight,
           )
         ],
       ),
--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.color1, this.customWidget});
+  ReusableCard({@required this.color1, this.customWidget, this.onPressed});
 
   final Color color1;
   final Widget customWidget;
+  final Function onPressed;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: customWidget,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: color1,
-        borderRadius: BorderRadius.circular(10.0),
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        child: customWidget,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: color1,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
       ),
     );
   }
